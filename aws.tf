f5dcb29b22836d7d188e440db8d1c909f618d475
Lowe Schmidt <github@loweschmidt.se>
2016-10-02 19:04:45 +0200

    revert back to elb, i know them better

--- aws.tf
+++ aws.tf
@@ -4,7 +4,6 @@ provider "aws" {
 }
 
 module "vpc" {
-  name            = "demo"
   source          = "github.com/terraform-community-modules/tf_aws_vpc"
   cidr            = "${var.vpc_cidr}"
   private_subnets = "${var.private_subnets}"
@@ -12,8 +11,24 @@ module "vpc" {
   azs             = "${var.azs}"
 }
 
-resource "aws_alb" "demo" {
-  subnets = "${mod.vpc.public_subnets}"
+resource "aws_elb" "demo" {
+  availability_zones = "${var.azs}"
+  instances          = "[${aws_instance.demo.*.id}]"
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healty_threshold    = 2
+    unhealthy_threshold = 3
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
 
   tags {
     Environment = "demo"
@@ -21,6 +36,48 @@ resource "aws_alb" "demo" {
   }
 }
 
+resource "aws_security_group" "elb_sg" {
+  vpc_id  = "${module.vpc.vpc_id}"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = "${var.allowed_cidrs}"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = "${var.allowed_cidrs}"
+  }
+}
+
 resource "aws_instance" "demo" {
   count = "${length(${var.azs})}"
+}
+
+resource "aws_security_group" "instance_sg" {
+  vpc_id = "${module.vpc.vpc_id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = "${var.allowed_cidrs}"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = "${var.vpc_cidr}"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = "${var.allowed_cidrs}"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,10 @@
-variable "vpc_cidr"        { }
-variable "region"          { }
-variable "aws_profile"     { }
-variable "azs"             { type = "list" }
-variable "private_subnets" { type = "list" }
-variable "public_subnets"  { type = "list" }
+variable "vpc_cidr"          { }
+variable "region"            { }
+variable "aws_profile"       { }
+variable "azs"               { type = "list" }
+variable "private_subnets"   { type = "list" }
+variable "public_subnets"    { type = "list" }
+variable "allowed_cidrs" {
+  type    = list
+  default = [ "0.0.0.0/0" ]
+}
